9880ddeaab6cef1296ead951890dc94c8595e6e9
Matthew Ogden <ogden12@gmail.com>
2021-04-05 17:39:38 -0500

    update create_venv

--- create_venv.sh
+++ create_venv.sh
@@ -1,5 +1,5 @@
-python3 -m venv capstone_env
+python3.6 -m venv capstone_env
 source capstone_env/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install numpy
-python3 -m pip install mpi4py
+python3.6 -m pip install --upgrade pip
+python3.6 -m pip install numpy
+python3.6 -m pip install mpi4py
