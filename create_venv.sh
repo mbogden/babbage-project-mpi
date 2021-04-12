eb65ca7a00ef6cb883674966f1b3884fe66d37c6
Matthew Ogden <ogden12@gmail.com>
2021-04-12 18:02:12 -0500

    Working draft that sorts correctly

--- create_venv.sh
+++ create_venv.sh
@@ -3,3 +3,4 @@ source capstone_env/bin/activate
 python3.6 -m pip install --upgrade pip
 python3.6 -m pip install numpy
 python3.6 -m pip install mpi4py
+python3.6 -m pip install pandas
--- mpi_sort.py
+++ mpi_sort.py
@@ -1,9 +1,13 @@
 # Code taken and modified from:
 # https://mpi4py.readthedocs.io/en/stable/tutorial.html
  
-from sys import argv
+from sys import argv, exit
 import numpy as np
+import pandas as pd
 from mpi4py import MPI
+
+# For troubleshooting
+import time
 
 # Global variable for now
  
@@ -16,29 +20,42 @@ print( 'I am rank %d of %d' % ( rank, size ) )
  
 # Create data to broadcast
 send_data = None
+pd_send_data = None
 size_col = np.zeros( 2, dtype=np.uint64 )
 
 if rank == 0:
 	print('input data: %s' % argv[1])
-	send_data = np.loadtxt( argv[1], delimiter=',', dtype=np.float32 )
-	print('Node 0 sending: \n',send_data,'\n')
+
+	pd_in = pd.read_csv( argv[1], header=None ).to_numpy( dtype=np.float32, copy=True )
+
+	# Force row_major
+	pd_send_data = np.empty( pd_in.shape, dtype=np.float32 )
+	pd_send_data[:,:] = pd_in[:,:]
+	del pd_in
+
+	np_send_data = np.loadtxt( argv[1], delimiter=',', dtype=np.float32 )
+
+	#print('Node 0 read in numpy: \n', np_send_data.flags,'\n', np_send_data, '\n')
+	print('Node 0 read in pandas: \n', pd_send_data.flags,'\n', pd_send_data, '\n')
 	
 	# Rank 0 calculates how much data is going to each node, and which array to sort by
 
-	n_per_node = round( 2 * float(send_data.shape[0]) / float(size) )
+	n_per_node = round( 2 * float(pd_send_data.shape[0]) / float(size) )
 	sort_col = int( argv[2] )
 
-	print( 'Size of input data: ', send_data.shape[0] )
+	print( 'Size of input data: ', pd_send_data.shape[0] )
 	print( 'N per node ', n_per_node )
 	print( 'Sort by col: ', sort_col )
 
 	size_col[0] = n_per_node
 	size_col[1] = sort_col
-	print( 'Sending ot all: ',size_col)
+	print( 'Sending to all: ',size_col)
 
 
 # Rank 0 will do a "one to all" call saying how much data to expect, and which array to sort by
 size_col = comm.bcast( size_col, root = 0 )
+comm.Barrier()
+time.sleep( rank*0.25 )
 print( 'Rank %d received: %d - %d'%(rank, size_col[0], size_col[1]))
 my_n = size_col[0]
 sort_col = size_col[1]
@@ -47,101 +64,131 @@ sort_col = size_col[1]
 my_array = np.ones( ( my_n, 4 ), dtype=np.float32 ) * np.nan
 
 # Rank 0 is scattering the initial data to all nodes
-comm.Scatter( send_data, my_array, root=0 )
-print( my_array )
+#send_data = np.transpose( send_data )
+comm.Scatter( pd_send_data, my_array, root=0 )
+
+comm.Barrier()
+time.sleep( rank*0.25 )
+print( 'Rank %d received my_array: \n'%rank, my_array )
 
 # TODO: Each node will organize there array and determine which items are being sent where
-def get_count_to_nodes():
+def bin_my_array( my_array ):
 
 	index_to_nodes = []
 	for i in range( size ):
 		index_to_nodes.append([])
 
 	bins = np.linspace( 0, 1, size +1 )
+	data_to_nodes = []
 
-	# From Dr. Wallin
-	'''
-	i = 1
-	a = np.random.rand(100,4)
-	b = a[ ( a[:,i] < 0.4 ) & a[:,i] >= 0.2 ) ]
-	'''
-
-	# TODO:  Be sure to ignore NAN in array!
-	for i in range( my_array.shape[0] ):
-		val = my_array[i,sort_col]
-
-		# TODO Calc which node this value should go to
-		to_node = i % size
-
-		index_to_nodes[to_node].append(i)
-
-	return index_to_nodes
-
-index_to_nodes = get_count_to_nodes()
-count_to_nodes = np.zeros( size, dtype=np.uint64 )
-
-if rank == 0:
 	for i in range( size ):
-		print(" Rank %d Going to node %d: "%(rank,i), index_to_nodes[i] )
+		data_to_nodes.append( my_array[(my_array[:,sort_col] < bins[i+1]) & (my_array[:,sort_col] >= bins[i])] )
+	
+	time.sleep( rank*0.25 )
 
-# Create integer array of how many items are going to each node
-for i in range( size):
-	count_to_nodes[i] = len( index_to_nodes[i] )
-print( count_to_nodes )
+	#'''
+	# For initial printing and code development
+	print( 'Rank %d array:'%rank)
+	print( my_array )
+	for i in range( size ):
+		print( '%d -> %d' % ( rank, i ) )
+		print( data_to_nodes[i] )
+	#'''
 
-# Make all to all mpi call passing of number of items
-# All nodes should know how many items they're getting
-# TODO: Make sure this is sending the correct amount to each person
-count_from_nodes = comm.alltoall( count_to_nodes )
-print( count_from_nodes )
+	return data_to_nodes
+
+data_to_nodes = bin_my_array( my_array )
 
 # Clean up my array.
 my_array *= np.nan
 
 
-# TODO:  Adjust my_array size to hold all the data
+# Create integer array of how many items are going to each node
+count_to_nodes = np.zeros( size, dtype=np.uint64 )
+for i in range( size):
+	count_to_nodes[i] = len( data_to_nodes[i] )
+
+comm.Barrier()
+time.sleep( rank*0.25 )
+print( 'Rank %d count_to_nodes: ' % rank, count_to_nodes )
+
+# Make all to all mpi call passing of number of items
+# All nodes should know how many items they're getting
+count_from_nodes = comm.alltoall( count_to_nodes )
+
+# Test Print
+comm.Barrier()
+time.sleep( rank*0.25 )
+print( 'Rank %d count_from_nodes: '%rank, count_from_nodes )
+
 my_n = np.sum( count_from_nodes )
 if my_n > my_array.shape[0]:
 	print("Rank %d my_array not big enough!: %d"%(rank, my_n) )
+	np.delete( my_array )
+	my_array = np.ones( ( my_n, 4 ), dtype=np.float32 ) * np.nan
+
 # TODO: Add items to your own large array again.
 
+# Calc where my data should be saved
+if rank == 0:
+	my_i = 0
+else:
+	my_i = int( np.sum( count_from_nodes[0:rank] ) )
+
+
+comm.Barrier()
+time.sleep( rank*0.25 )
+print( 'Rank %d saving own data to index: %d : %d' %( rank, int( my_i ), int( my_i + count_from_nodes[rank] ) ) )
+my_array[ int(my_i) : int( my_i + count_from_nodes[rank] ), : ] = data_to_nodes[ rank ][:,:]
+print( 'Rank %d saving own data: \n' % rank, my_array )
+#my_array = data_to_nodes[ rank ]
+
+
+comm.Barrier()
+time.sleep( rank )
 
 # TODO:  Loop through nodes, sending the subsection of array to node
 
 # Just practice with rank 1 sending to rank 2 for now
-#for i in range( size ):
-if rank == 1:
+for i in range( size ):
+	#if rank == 1:
 	# Skip self
-
-	# TODO: Construct temporary array to send to everyone
-	temp_array_1 = np.ones( ( count_to_nodes[1] , 4 )) * rank
+	if i == rank:
+		continue
 
 	# Send the array, and move on
-	comm.isend( temp_array_1, dest=2, tag=77 )
+	comm.isend( data_to_nodes[i], dest=i, tag=25 )
 
-	print("Rank 1 sent to node 2: \n",temp_array_1)
+	print( "Rank %d to %d: \n"%(rank, i),data_to_nodes[i] )
 
-# TODO: Create temporary array to receive.
-temp_array_2 = np.ones( ( np.amax(count_from_nodes), 4 ), dtype=np.float32 ) * np.nan
-
+comm.Barrier()
+time.sleep( rank )
 
 # TODO:  Loop through nodes, gathering the subsections of arrays from each node and place in one large array
 
-#for i in range( size ):
-if rank == 2:
+my_i = 0
+for i in range( size ):
 
 	# Skip self
+	if rank == i:
+		my_i += count_from_nodes[i]
+		continue
 	
-	temp_array_2 = comm.recv( source=1, tag=77 )
+	my_array[ int(my_i) : int(my_i + count_from_nodes[i]), : ] = comm.recv( source=i, tag=25 )
+	my_i += count_from_nodes[i]
 
-	print("Rank 2 received from rank 1:\n", temp_array_2)
+	print("Rank %d from %d:\n"%(rank,i), my_array )
 
-	# TODO: Add subarray to large array and move to next receive
+my_array = my_array[ 0 : my_n , : ]
 
-	# my_array[ something : something+size of temp array ] = temp_array_2 
+comm.Barrier()
+time.sleep( rank*0.25 )
+print( "Rank %d will sort: \n "%rank, my_array )
 
-	# del temp_array
+my_array = my_array[ np.argsort( my_array[:, sort_col] ) ]
 
 
-# TODO:  Sort large array.
+comm.Barrier()
+time.sleep( rank*0.25 )
+print( "Rank %d sorted array!: \n "%rank, my_array )
 
