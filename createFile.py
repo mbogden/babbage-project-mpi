174f607cff48d090bbdafb269795ed8c5c7ab085
Matthew Ogden <ogden12@gmail.com>
2021-04-14 16:08:15 -0500

    fixed create file

--- createFile.py
+++ createFile.py
@@ -5,7 +5,7 @@ np.random.seed( 1212 )
 n = int( argv[1] )
 saveFile = argv[2]
 
-oFile = open( 'data_2.csv', 'w' )
+oFile = open( saveFile, 'w' )
 
 for i in range( n ):
 	line = '%d,%f,%f,%f\n' % ( i, np.random.rand(), np.random.rand(), np.random.rand() )
